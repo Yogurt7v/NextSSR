9c6483f2960ec3903e718600c94595c4c7e9648c
Yogurt7v <egorov-2k@yandex.ru>
2025-07-17 21:09:49 +0300

    some change

--- src/app/products/[...slug]/page.tsx
+++ src/app/products/[...slug]/page.tsx
@@ -1,8 +1,23 @@
+// const getServerSideProps = () => {
+//   return { props: { name: 'boeing' } };
+// };
 'use client';
-import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function ProductName() {
   const pathname = usePathname();
-  console.log(pathname.split('/'));
-  return <div>Product Name</div>;
+  const search = useSearchParams();
+  const links = pathname.split('/');
+  console.log(search);
+  return (
+    <div>
+      Product Name
+      {links.map((item, index) => (
+        <div className="flex flex-col gap-3" key={index}>
+          <Link href={''}>{item}</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
---
+++ src/app/products/cars/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductCars() {
+  return <div>Cars</div>;
+}
