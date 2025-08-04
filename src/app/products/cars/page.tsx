64fedf4e737269c7b70e73c332c66d69d3245f1a
Yogurt7v <egorov-2k@yandex.ru>
2025-08-04 20:20:03 +0300

    generateMetadata

--- src/app/products/cars/page.tsx
+++ src/app/products/cars/page.tsx
@@ -1,3 +1,12 @@
+export async function generateMetadata() {
+  const product = await fetch(`https://jsonplaceholder.typicode.com/users/1`).then(
+    (res) => res.json()
+  );
+
+  return {
+    title: product.name,
+  };
+}
 export default function ProductCars() {
   return <div>Cars</div>;
 }
