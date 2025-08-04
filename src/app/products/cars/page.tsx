cd79f1361131cc728c66f2facbab646561061702
Yogurt7v <egorov-2k@yandex.ru>
2025-08-04 20:24:32 +0300

    some fix

--- src/app/products/cars/page.tsx
+++ src/app/products/cars/page.tsx
@@ -1,7 +1,9 @@
 export async function generateMetadata() {
-  const product = await fetch(`https://jsonplaceholder.typicode.com/users/1`).then(
-    (res) => res.json()
-  );
+  const randomNumber = Math.floor(Math.random() * (10 - 1) + 1);
+  console.log(randomNumber);
+  const product = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${randomNumber}`
+  ).then((res) => res.json());
 
   return {
     title: product.name,
