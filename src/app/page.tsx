a6a6af6d6d86fa99ba585d1398f2e2c4aa8ae549
Yogurt7v <egorov-2k@yandex.ru>
2025-08-06 20:35:32 +0300

    создан единый сервак. и функция получения всех юзеров

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import prisma from '@/server/db';
+
 export default async function Home() {
+  const data = await prisma.user.findMany();
+
   return (
     <>
-      <div>Test</div>
+      {/* <div>Test</div> */}
+      {data.map((item, index) => (
+        <p key={index}>{item.name}</p>
+      ))}
     </>
   );
 }
